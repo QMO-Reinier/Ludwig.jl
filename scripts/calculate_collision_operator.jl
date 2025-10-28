752a92c5088e17569d3d8c6ddf884406931a354d
Reinier Regter <r.q.regter@umail.leidenuniv.nl>
2025-10-28 17:47:17 +0100

    update

--- scripts/calculate_collision_operator.jl
+++ scripts/calculate_collision_operator.jl
@@ -1,5 +1,5 @@
 include("electron_electron.jl")
-include("/home/lion/Desktop/simulation_code/Code_scaffidi/scripts/materials/Sr2RuO4.jl")
+include("materials/Sr2RuO4.jl")
 using PlotlyJS
 using LaTeXStrings
 using LinearAlgebra
@@ -10,10 +10,10 @@ n_E_bins      = 4  # number of energy bins
 n_theta_bins  = 6 # number of angular bins
 annular_width = 6   # 2*annular_width*kbT = width around Fermi surface
 material_file = "Sr2RuO4.jl"
-data_dir   = "/home/lion/Desktop/simulation_code/Code_scaffidi/data"
+data_dir   = "//file/home_lion/Regter/Phd/GitHub/Hydro/cuprates_transport/Ludwig.jl/data/"
 
 bands = [ham_α, ham_β, ham_γ]
 
-outfile = joinpath(@__DIR__, data_dir, "test_data.h5")
+outfile = joinpath(@__DIR__, data_dir, "test_data_v2.h5")
 
 main(T, n_E_bins, n_theta_bins, outfile, bands, annular_width)
